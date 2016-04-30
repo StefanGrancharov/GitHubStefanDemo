7a1bdebd44379205ee3f6a47769329be801b66fd
StefanGrancharov <stefan.grancharov@gmail.com>
2016-04-30 20:44:06 +0300

    Solved second task

---
+++ ConsoleApplication1/DemoTasktwo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTasktwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Second Demo Task
+            //output
+            Console.WriteLine("Second task!");
+        }
+    }
+}
---
+++ ConsoleApplication1/Second Task/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Second task
+            //output
+            Console.WriteLine("Second task!");
+        }
+    }
+}
